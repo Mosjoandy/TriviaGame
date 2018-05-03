237645a29013b93bf0f609c248f48720deecdf95
Mosjoandy <34078510+Mosjoandy@users.noreply.github.com>
2018-05-02 21:32:55 -0700

    made forloop to make new questions, made start and end screens, cleaned up some code and made functions instead because DRY

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,11 @@
 // Making the timer
-var timeLimit = 30;
+var timeLimit = 10;
 var intervalId;
 
 // creating functions for timer
 // Start the timer
 function runTimer() {
-    timeLimit = 30;
+    timeLimit = 10;
     intervalId = setInterval(decrement, 1000);
 }
 
@@ -21,12 +21,14 @@ function decrement() {
 
     //Time limit stops the countdown
     if (timeLimit === 0) {
+        
         stop();
-
-        // GO TO NEXT QUESTION
-    } else {
-        omitted++;
-    }
+        destroy();
+        questionCounter++;
+        nextQuestion(questionCounter);
+        omitted++;        
+   
+    } 
 }
 
 // data vars
@@ -34,40 +36,105 @@ var correct = 0;
 var incorrect = 0;
 var omitted = 0;
 
-//arrays with questions, answers, and right answers
-var questions = ["What is my name?", "What is your name?"]
-var answers = {
-    question1: ["Mel", "David", "Chris", "Nick"],
-    question2: ["Marcella", "Marisa", "Jontue", "Janet"]
-}
-var rightanswers = ["Nick", "Marcella"]
+// question bank with answers
+var gameData = [
+    {
+        question: "question 1",
+        choices: ["Lose", "Lose", "Lose", "Win"],
+        answer: 3
+    },
+    {
+        question: "question 2",
+        choices: ["Win", "Lose", "Lose", "Lose"],
+        answer: 0
+    },
+    {
+        question: "question 3",
+        choices: ["Lose", "Win", "Lose", "Lose"],
+        answer: 1
+    }
+]
 
-// correct answer function
-function correctChoice() {
-    correct++;
-}
-
-// wrong answer function
-function incorrectChoice() {
-    incorrect++;
-}
-
-// function that prints questions to the screen
-function showQuestions() {
-    $(".questionArea").html(questions[0])
-}
-
-// functino that prints answers to the screen
-function showAnswers() {
-    $(".answerArea").html(  "<p>A. " + answers.question1[0] + "</p>" +
-                            "<p>B. " + answers.question1[1] + "</p>" +
-                            "<p>C. " + answers.question1[2] + "</p>" +
-                            "<p>D. " + answers.question1[3] + "</p>")
+// function that clears the choice and question areas
+function destroy() {
+    $(".choiceArea").empty();
+    $(".questionArea").empty();
 }
 
-// function that will run forloop and show answers
-function loopAnswers() {
-    for (var i=0; i < answers.question1.length; i++) {
+// var that keeps track of object++
+var questionCounter = 0;
+
+function nextQuestion(questionCounter) {
+    // Display next question in questionArea
+    $(".questionArea").append(gameData[questionCounter].question);
+
+    // forloop for questionCounter increments
+    for (var i=0; i < gameData[questionCounter].choices.length; i++) {
+        
+        //Make a new container
+        var container = $("<div>");
+        //give that container a class="choices"
+        container.attr("class", "choices");
+        // give that container a data value of i
+        container.attr("data-value", i);
+        // display the choices for the user
+        container.text(gameData[questionCounter].choices[i]);
+        // hook for choiceArea and append the container just made
+        $(".choiceArea").append(container);
+      
+    }
+}
+
+// When user clicks on one of the choices
+$(document).on("click", ".choices", function() {
+    //assign a value to data-value
+    var answerChoice = $(this).attr("data-value");
+    //convert the value into an integer
+    answerChoice = parseInt(answerChoice);
+        
+        //if the answer chosen is the same as the answer integer, show next question,
+        //increase correct or incorrect accordingly
+        if (answerChoice === gameData[questionCounter].answer) {
+            console.log("you picked right")
+            destroy();
+            nextQuestion(questionCounter);    
+            stop();
+            correct++;
+                        
+        } else {
+            console.log("you picked wrong")
+            destroy();
+            nextQuestion(questionCounter);
+            stop();
+            incorrect++;    
+        }
+})
+
+// function for starting the game
+function startScreen() {
+    $(".startScreen").html("<h1>Click to Start</h1>").on("click", function() {
+        runTimer();
+        nextQuestion(questionCounter);
+        $(".startScreen").empty();
+    })
+}
+
+//function for showing scores and ending game
+function endScreen() {
+    stop();
+    destroy();
+    $(".timer").empty();
+        //display scores with append
+    $(".startScreen").html("<h1>Correct: " + correct + "</h1>")
+    $(".startScreen").append("<h1>Incorrect: " + incorrect + "</h1>")
+    $(".startScreen").append("<h1>Omitted: " + omitted  + "</h1>")
     
 }
+
+// BEGIN THE GAME!
+startScreen();
+
+// Show scores and end the game
+if (questionCounter === 1) {
+    endScreen();
 }
